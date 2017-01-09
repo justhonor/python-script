181ecbabcaf5f37eaa6b81f8c2c1a5f1bf648a3f
Aiapple <zx_zhang03@163.com>
2017-01-09 10:46:24 -0500

    debug

--- cp_info_into_file/cp_info_into_file.py
+++ cp_info_into_file/cp_info_into_file.py
@@ -45,7 +45,7 @@ def process_info(destfile):
 
 		 # Is heading add four '#' 
 		 if line[0].isupper():
-			 line=line[:0]+"### "+line[0:]
+			 line=line[:0]+"###"+line[0:]
 		
 
 		 # Write to README.md
